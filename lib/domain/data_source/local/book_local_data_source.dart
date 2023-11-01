c1995091b23ed2d5c16a85fa1633cf7f971e1cea
Vladlen <vjladjleh@gmail.com>
2023-11-01 19:49:15 +0400

    fix(7): change getBooks() return type

--- lib/domain/data_source/local/book_local_data_source.dart
+++ lib/domain/data_source/local/book_local_data_source.dart
@@ -3,7 +3,7 @@ import 'package:books/domain/model/model.dart';
 abstract interface class IBookLocalDataSource {
   Future<void> upsertBooks(List<BookModel> books);
 
-  Future<Iterable<BookModel>> getBooks({required QueryParameters queryParameters});
+  Future<List<BookModel>> getBooks({required QueryParameters queryParameters});
 
   Future<void> addBookToFavourites({required int userId, required String bookId});
   Future<void> deleteBookFromFavourites({required int userId, required String bookId});
