241d45eebb073520073b1e5d0de9b935eaa9c348
Vladlen <vjladjleh@gmail.com>
2023-11-06 21:47:29 +0400

    fix(8): add network connection state

--- lib/presentation/network_connection_cubit/network_connection_cubit.dart
+++ lib/presentation/network_connection_cubit/network_connection_cubit.dart
@@ -2,18 +2,25 @@ import 'dart:async';
 
 import 'package:books/domain/service/connectivity_service.dart';
 import 'package:books/domain/model/enum/connection_status.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ConnectionCubit extends Cubit<ConnectionStatus> {
-  ConnectionCubit({
+part 'network_connection_state.dart';
+
+class NetworkConnectionCubit extends Cubit<NetworkConnectionState> {
+  NetworkConnectionCubit({
     required IConnectivityService connectivityService,
-  }) : super(connectivityService.status) {
+  }) : super(NetworkConnectionState(status: connectivityService.status)) {
     _statusSubscription = connectivityService.statusStream.listen((ConnectionStatus status) {
-      emit(status);
+      _statusChanged(status);
     });
   }
 
   late final StreamSubscription<ConnectionStatus> _statusSubscription;
+
+  void _statusChanged(ConnectionStatus status) {
+    emit(NetworkConnectionState(status: status));
+  }
 
   @override
   Future<void> close() {
--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,7 +1,7 @@
 //GENERATED BARREL FILE
 export './auth/auth.dart';
-export './connection_cubit/connection_cubit.dart';
 export './favorites_bloc/favorites_bloc.dart';
 export './navigation_cubit/navigation_cubit.dart';
+export './network_connection_cubit/network_connection_cubit.dart';
 export './search_bloc/search_bloc.dart';
 export './user_auth_bloc/user_auth_bloc.dart';
