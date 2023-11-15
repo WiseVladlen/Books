958d6a18f27c535afbb7927f8478c5131ec70a93
Vladlen <vjladjleh@gmail.com>
2023-11-15 18:19:21 +0400

    fix: add cache data source methods

--- lib/domain/data_source/cache_data_source.dart
+++ lib/domain/data_source/cache_data_source.dart
@@ -2,6 +2,11 @@ import 'package:books/domain/model/model.dart';
 
 abstract interface class ICacheDataSource {
   UserModel? readUser();
-
   Future<void> writeUser(UserModel? value);
+
+  String? readThemeMode();
+  Future<void> writeThemeMode(String? value);
+
+  String? readLanguageCode();
+  Future<void> writeLanguageCode(String? value);
 }
