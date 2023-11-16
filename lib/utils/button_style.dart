9fe24c92a2007959600d3e9c6985d4fcbe51e482
Vladlen <vjladjleh@gmail.com>
2023-11-16 12:38:08 +0400

    fix: add button style

--- lib/utils/button_style.dart
+++ lib/utils/button_style.dart
@@ -10,23 +10,40 @@ class ButtonStyles extends ThemeExtension<ButtonStyles> {
       foregroundColor: MaterialStatePropertyAll<Color>(Color(0xFFEF5350)),
       overlayColor: MaterialStatePropertyAll<Color>(Color(0x1FEF5350)),
     ),
+    this.neutralButton = const ButtonStyle(
+      foregroundColor: MaterialStatePropertyAll<Color>(Colors.black),
+      overlayColor: MaterialStatePropertyAll<Color>(Color(0x0f000000)),
+    ),
   });
 
   const ButtonStyles.light() : this();
 
-  const ButtonStyles.dark() : this();
+  const ButtonStyles.dark()
+      : this(
+          negativeDialogButton: const ButtonStyle(
+            foregroundColor: MaterialStatePropertyAll<Color>(Color(0xFFFF5252)),
+            overlayColor: MaterialStatePropertyAll<Color>(Color(0x1FF27573)),
+          ),
+          neutralButton: const ButtonStyle(
+            foregroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+            overlayColor: MaterialStatePropertyAll<Color>(Color(0x1FFFFFFF)),
+          ),
+        );
 
   final ButtonStyle positiveDialogButton;
   final ButtonStyle negativeDialogButton;
+  final ButtonStyle neutralButton;
 
   @override
   ThemeExtension<ButtonStyles> copyWith({
     ButtonStyle? positiveDialogButton,
     ButtonStyle? negativeDialogButton,
+    ButtonStyle? neutralButton,
   }) {
     return ButtonStyles(
       positiveDialogButton: positiveDialogButton ?? this.positiveDialogButton,
       negativeDialogButton: negativeDialogButton ?? this.negativeDialogButton,
+      neutralButton: neutralButton ?? this.neutralButton,
     );
   }
 
@@ -37,6 +54,7 @@ class ButtonStyles extends ThemeExtension<ButtonStyles> {
     return ButtonStyles(
       positiveDialogButton: ButtonStyle.lerp(positiveDialogButton, other.positiveDialogButton, t)!,
       negativeDialogButton: ButtonStyle.lerp(negativeDialogButton, other.negativeDialogButton, t)!,
+      neutralButton: ButtonStyle.lerp(neutralButton, other.neutralButton, t)!,
     );
   }
 }
